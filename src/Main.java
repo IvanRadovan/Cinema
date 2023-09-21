4ebeaef058068658372b7599606fd7c44e5ef227
IvanRadovan <ivano-radovanovic@hotmail.com>
2023-09-22 00:32:58 +0200

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
+import se.nackademin.cinema.Cinema;
+
 public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        Cinema cinema = new Cinema("Muana", 5,10);
+
+        System.out.println(cinema.getSeats());
+    }
 }
